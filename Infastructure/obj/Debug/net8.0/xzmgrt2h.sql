371309ea0f0e15d72d09286f4c8596af11841e69
rich-f-p <richardferry3@gmail.com>
2024-10-31 21:14:32 -0400

    remodel file folder

---
+++ Infastructure/obj/Debug/net8.0/xzmgrt2h.sql
@@ -0,0 +1,425 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [Casts] (
+        [Id] int NOT NULL IDENTITY,
+        [Gender] nvarchar(MAX) NOT NULL,
+        [Name] nvarchar(128) NOT NULL,
+        [ProfilePath] nvarchar(2084) NOT NULL,
+        [TmdbUrl] nvarchar(MAX) NOT NULL,
+        CONSTRAINT [PK_Casts] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [Genres] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(24) NOT NULL,
+        CONSTRAINT [PK_Genres] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [Movies] (
+        [Id] int NOT NULL IDENTITY,
+        [BackdropUrl] nvarchar(2084) NULL,
+        [Budget] decimal(18,4) NULL,
+        [CreatedBy] nvarchar(MAX) NULL,
+        [CreatedDate] datetime2 NULL,
+        [ImdbUrl] nvarchar(2084) NULL,
+        [OriginalLanguage] nvarchar(64) NULL,
+        [Overview] nvarchar(MAX) NULL,
+        [PosterUrl] nvarchar(2084) NULL,
+        [Price] decimal(5,2) NULL,
+        [ReleaseDate] datetime2 NULL,
+        [Revenue] decimal(18,4) NULL,
+        [RunTime] int NULL,
+        [Tagline] nvarchar(512) NULL,
+        [Title] nvarchar(256) NULL,
+        [TmdbUrl] nvarchar(2084) NULL,
+        [UpdatedBy] nvarchar(MAX) NULL,
+        [UpdatedDate] datetime2 NULL DEFAULT (getdate()),
+        CONSTRAINT [PK_Movies] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [Roles] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(20) NOT NULL,
+        CONSTRAINT [PK_Roles] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [Users] (
+        [Id] int NOT NULL IDENTITY,
+        [DateOfBirth] datetime2 NOT NULL,
+        [Email] nvarchar(256) NOT NULL,
+        [FirstName] nvarchar(128) NOT NULL,
+        [HashedPassword] nvarchar(1024) NOT NULL,
+        [IsLocked] int NULL,
+        [LastName] nvarchar(128) NOT NULL,
+        [PhoneNumber] nvarchar(16) NULL,
+        [ProfilePictureUrl] nvarchar(MAX) NULL,
+        [Salt] nvarchar(1024) NOT NULL,
+        CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [MovieCasts] (
+        [CastId] int NOT NULL,
+        [Character] nvarchar(450) NOT NULL,
+        [MovieId] int NOT NULL,
+        CONSTRAINT [PK_MovieCasts] PRIMARY KEY ([CastId], [Character], [MovieId]),
+        CONSTRAINT [FK_MovieCasts_Casts_CastId] FOREIGN KEY ([CastId]) REFERENCES [Casts] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_MovieCasts_Movies_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movies] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [MovieGenres] (
+        [GenreId] int NOT NULL,
+        [MovieId] int NOT NULL,
+        CONSTRAINT [PK_MovieGenres] PRIMARY KEY ([GenreId], [MovieId]),
+        CONSTRAINT [FK_MovieGenres_Genres_GenreId] FOREIGN KEY ([GenreId]) REFERENCES [Genres] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_MovieGenres_Movies_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movies] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [Trailers] (
+        [Id] int NOT NULL IDENTITY,
+        [MovieId] int NOT NULL,
+        [Name] nvarchar(2084) NOT NULL,
+        [TrailerUrl] nvarchar(2084) NOT NULL,
+        CONSTRAINT [PK_Trailers] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Trailers_Movies_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movies] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [Favorites] (
+        [MovieId] int NOT NULL,
+        [UserId] int NOT NULL,
+        CONSTRAINT [PK_Favorites] PRIMARY KEY ([MovieId], [UserId]),
+        CONSTRAINT [FK_Favorites_Movies_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movies] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Favorites_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [Purchases] (
+        [MovieId] int NOT NULL,
+        [UserId] int NOT NULL,
+        [PurchaseDateTime] datetime2 NOT NULL,
+        [PurchaseNumber] nvarchar(450) NOT NULL,
+        [TotalPrice] decimal(5,2) NOT NULL,
+        CONSTRAINT [PK_Purchases] PRIMARY KEY ([MovieId], [UserId]),
+        CONSTRAINT [FK_Purchases_Movies_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movies] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Purchases_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [Reviews] (
+        [MovieId] int NOT NULL,
+        [UserId] int NOT NULL,
+        [CreatedDate] datetime2 NOT NULL,
+        [Rating] decimal(3,2) NOT NULL,
+        [ReviewText] nvarchar(MAX) NOT NULL,
+        CONSTRAINT [PK_Reviews] PRIMARY KEY ([MovieId], [UserId]),
+        CONSTRAINT [FK_Reviews_Movies_MovieId] FOREIGN KEY ([MovieId]) REFERENCES [Movies] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Reviews_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE TABLE [UserRoles] (
+        [RoleId] int NOT NULL,
+        [UserId] int NOT NULL,
+        CONSTRAINT [PK_UserRoles] PRIMARY KEY ([RoleId], [UserId]),
+        CONSTRAINT [FK_UserRoles_Roles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [Roles] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_UserRoles_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [Users] ([Id]) ON DELETE CASCADE
+    );
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE INDEX [IX_Favorites_UserId] ON [Favorites] ([UserId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE INDEX [IX_MovieCasts_MovieId] ON [MovieCasts] ([MovieId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE INDEX [IX_MovieGenres_MovieId] ON [MovieGenres] ([MovieId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE UNIQUE INDEX [IX_Purchases_PurchaseNumber] ON [Purchases] ([PurchaseNumber]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE INDEX [IX_Purchases_UserId] ON [Purchases] ([UserId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE INDEX [IX_Reviews_UserId] ON [Reviews] ([UserId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE INDEX [IX_Trailers_MovieId] ON [Trailers] ([MovieId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    CREATE INDEX [IX_UserRoles_UserId] ON [UserRoles] ([UserId]);
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241028225918_Init'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20241028225918_Init', N'8.0.10');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241031023946_default_UserSalt_Value'
+)
+BEGIN
+    DECLARE @var0 sysname;
+    SELECT @var0 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Users]') AND [c].[name] = N'Salt');
+    IF @var0 IS NOT NULL EXEC(N'ALTER TABLE [Users] DROP CONSTRAINT [' + @var0 + '];');
+    ALTER TABLE [Users] ADD DEFAULT N'salt' FOR [Salt];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241031023946_default_UserSalt_Value'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20241031023946_default_UserSalt_Value', N'8.0.10');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241031083333_set_default_values_movie_and_user'
+)
+BEGIN
+    DECLARE @var1 sysname;
+    SELECT @var1 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Users]') AND [c].[name] = N'IsLocked');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Users] DROP CONSTRAINT [' + @var1 + '];');
+    ALTER TABLE [Users] ADD DEFAULT 0 FOR [IsLocked];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241031083333_set_default_values_movie_and_user'
+)
+BEGIN
+    DECLARE @var2 sysname;
+    SELECT @var2 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Movies]') AND [c].[name] = N'UpdatedBy');
+    IF @var2 IS NOT NULL EXEC(N'ALTER TABLE [Movies] DROP CONSTRAINT [' + @var2 + '];');
+    ALTER TABLE [Movies] ADD DEFAULT N'DataAdmin' FOR [UpdatedBy];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241031083333_set_default_values_movie_and_user'
+)
+BEGIN
+    DECLARE @var3 sysname;
+    SELECT @var3 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Movies]') AND [c].[name] = N'Price');
+    IF @var3 IS NOT NULL EXEC(N'ALTER TABLE [Movies] DROP CONSTRAINT [' + @var3 + '];');
+    ALTER TABLE [Movies] ADD DEFAULT 5.0 FOR [Price];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241031083333_set_default_values_movie_and_user'
+)
+BEGIN
+    DECLARE @var4 sysname;
+    SELECT @var4 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Movies]') AND [c].[name] = N'CreatedDate');
+    IF @var4 IS NOT NULL EXEC(N'ALTER TABLE [Movies] DROP CONSTRAINT [' + @var4 + '];');
+    ALTER TABLE [Movies] ADD DEFAULT (getdate()) FOR [CreatedDate];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241031083333_set_default_values_movie_and_user'
+)
+BEGIN
+    DECLARE @var5 sysname;
+    SELECT @var5 = [d].[name]
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Movies]') AND [c].[name] = N'CreatedBy');
+    IF @var5 IS NOT NULL EXEC(N'ALTER TABLE [Movies] DROP CONSTRAINT [' + @var5 + '];');
+    ALTER TABLE [Movies] ADD DEFAULT N'DataAdmin' FOR [CreatedBy];
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20241031083333_set_default_values_movie_and_user'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20241031083333_set_default_values_movie_and_user', N'8.0.10');
+END;
+GO
+
+COMMIT;
+GO
+
